d9327b9ae92b62fed38abdaa3aa31246a544de19
sudharshini23 <sudarsh25@gmail.com>
2023-07-30 10:43:12 +0530

    Expense controller transaction has errors

--- backend/controllers/expense.js
+++ backend/controllers/expense.js
@@ -1,5 +1,6 @@
 const Expense = require('../models/expense');
 const User = require('../models/user');
+const sequelize = require('../util/database');
 
 const getexpense = async (req,res,next) => {
     
@@ -16,51 +17,66 @@ const getexpense = async (req,res,next) => {
 
 const addexpense = async (req,res,next) => {
     console.log(req.body);
+    const t = await sequelize.transaction();
+
+try {
     const expenseamount = req.body.expenseamount;
     const description = req.body.description;
     const category = req.body.category;
-    Expense.create({
+    const expense = await Expense.create({
         expenseamount: expenseamount,
         description: description,
         category: category,
         userId: req.user.id
+    },
+    {
+        transaction: t
     })
-    .then(result => {
-        const totalExpense = Number(req.user.totalExpenses) + Number(expenseamount)
-        console.log(totalExpense)
-        User.update({
-            totalExpenses: totalExpense
-        }, {
-            where: {id: req.user.id}
-        }).then(async() => {
-            res.status(201).json({expenses: result})
-        })
-        .catch(async(err) => {
-            return res.status(500).json({success: false, error: err})
-        })
+    const totalExpense = Number(req.user.totalExpenses) + Number(expenseamount)
+    console.log(totalExpense)
+    await User.update({
+        totalExpenses: totalExpense
+    }, {
+        where: {id: req.user.id},
+        transaction: t
     })
-    .catch(err => {
+    await t.commit();
+    res.status(201).json({expenses: result})
+}
+    catch(err) {
+        await t.rollback();
         res.status(500).json({error: err})
-    })
+    }
 }
 
 const deleteexpense = async (req,res,next) => {
+    const t = await sequelize.transaction();
+    try {
+    // const expenseamount = req.body.expenseamount;
     const expenseId = req.params.id;
-    await Expense.findByPk(expenseId)
-    .then(expense => {
-        return expense.destroy({where: { id: expenseId, userId: req.user.id}});
-    })
-    .then(result => {
-        if(result === 0) {
-            return res.status(404).json({success: false, message: 'Expense doesnt belong to the user'})
-        }
-        else {
+    const expense = await Expense.findByPk(expenseId)
+    const delexpamt = expense.expenseamount;
+    const result = await expense.destroy({where: { id: expenseId, userId: req.user.id}, transaction: t});
+        // if(result === 0) {
+        //     await t.rollback();
+        //     return res.status(404).json({success: false, message: 'Expense doesnt belong to the user'})
+        // }
+        console.log(req.user.totalExpenses)
+        const newtotalExpense = Number(req.user.totalExpenses) - Number(delexpamt)
+        console.log(newtotalExpense);
+        await User.update({
+            totalExpenses: newtotalExpense
+        }, {
+            where: {id: req.user.id},
+            transaction: t
+        })
+            await t.commit();
             res.status(200).json({success: true, message: 'Deleted Successfully'})
-        }
-    })
-    .catch(err => {
+    }
+    catch(err) {
+        await t.rollback();
         res.status(500).json({error: err})
-    })
+    }
 }
 
 module.exports = {
